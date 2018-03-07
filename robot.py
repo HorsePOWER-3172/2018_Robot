20e26f8c08e792e85350b3015d1a071085ffa2ab
VergilTheHuragok <mathewsreece@gmail.com>
2018-03-06 20:44:50 -0600

    Initial Support for buttons and fixed speed caps. Initial work on locking wheels together

--- robot.py
+++ robot.py
@@ -1,29 +1,37 @@
+import time
+
 import wpilib
 import wpilib.drive
 
 # Gamepad axes
 
-LEFT_AXIS = 1
-RIGHT_AXIS = 5
+LEFT_WHEELS_AXIS = 1
+RIGHT_WHEELS_AXIS = 5
 LIFT_AXIS = 0  # Should be triggers
 ARM_AXIS = 4
 
+# If difference of wheel values is <=: Average two values
+WHEEL_LOCK = .1
+
 # Initiate Deadzone values
 DEFAULT_DEADZONE = .3
-DEADZONES = {LEFT_AXIS: DEFAULT_DEADZONE, RIGHT_AXIS: DEFAULT_DEADZONE,
-             LIFT_AXIS: .3, ARM_AXIS: DEFAULT_DEADZONE}
+DEADZONES = {LEFT_WHEELS_AXIS: DEFAULT_DEADZONE,
+             RIGHT_WHEELS_AXIS: DEFAULT_DEADZONE, LIFT_AXIS: .3,
+             ARM_AXIS: DEFAULT_DEADZONE}
 
 # Initiate reverse values
 DEFAULT_REVERSE = False
-REVERSES = {LEFT_AXIS: not DEFAULT_REVERSE, RIGHT_AXIS: DEFAULT_REVERSE,
-            LIFT_AXIS: DEFAULT_REVERSE, ARM_AXIS: DEFAULT_REVERSE}
+REVERSES = {LEFT_WHEELS_AXIS: not DEFAULT_REVERSE,
+            RIGHT_WHEELS_AXIS: DEFAULT_REVERSE, LIFT_AXIS: not DEFAULT_REVERSE,
+            ARM_AXIS: DEFAULT_REVERSE}
 
 # Initiate Speed caps
-DEFAULT_SPEED_CAP = [-.5, .5]
-SPEED_CAPS = {LEFT_AXIS: DEFAULT_SPEED_CAP, RIGHT_AXIS: DEFAULT_SPEED_CAP,
-              LIFT_AXIS: [-1, 1], ARM_AXIS: DEFAULT_SPEED_CAP}
+DEFAULT_SPEED_CAP = [.3, .3]
+SPEED_CAPS = {LEFT_WHEELS_AXIS: DEFAULT_SPEED_CAP,
+              RIGHT_WHEELS_AXIS: DEFAULT_SPEED_CAP, LIFT_AXIS: [.8, 0],
+              ARM_AXIS: [1, 1]}
 
-# Controller ports
+# PWM ports
 
 LEFT_PORT = 0
 RIGHT_PORT = 1
@@ -32,8 +40,17 @@ ARM_PORT = 3
 
 GAMEPAD_NUM = 0  # This should prolly always be zero
 
+# Button Ports
+REVERSE_BUTTON = 1
+LOCK_BUTTON = 3
 
-# TODO: Add speed caps and scales
+# Gamepad rumble length in milliseconds
+RUMBLE_LENGTH = 100
+
+
+def get_millis():
+    """Returns the current time in milliseconds"""
+    return time.time() * 1000
 
 
 class Robot(wpilib.IterativeRobot):
@@ -49,10 +66,23 @@ class Robot(wpilib.IterativeRobot):
         self.robot_lift = wpilib.PWMTalonSRX(LIFT_PORT)
         self.robot_arm = wpilib.PWMTalonSRX(ARM_PORT)
 
+        # Initiate button stuff
         self.buttons = set()
+        self.button_toggles = None
+        self.button_states = {}
+        self.rumble_time = None
+        self.reset_buttons()
 
         # Initiate gamepad
         self.game_pad = wpilib.Joystick(GAMEPAD_NUM)
+
+    def reset_buttons(self):
+        """Resets the values of the button toggles to default likewise
+        defined here
+        """
+        self.button_toggles = {"REVERSE": False, "LOCK": True}
+        self.button_states = {}
+        self.rumble_time = None
 
     def stop(self):
         """Stops all motors which are currently moving"""
@@ -74,7 +104,9 @@ class Robot(wpilib.IterativeRobot):
 
     # Events
     def robotInit(self):
-        """Runs at the same time as __init__. Appears to be redundant"""
+        """Runs at the same time as __init__. Appears to be redundant in this
+        case
+        """
         pass
 
     def robotPeriodic(self):
@@ -87,20 +119,56 @@ class Robot(wpilib.IterativeRobot):
         self.stop()
 
     def autonomousInit(self):
-        self.limits = set()
+        self.reset_buttons()
 
     def autonomousPeriodic(self):
         pass
 
     def teleopInit(self):
-        self.limits = set()
+        self.reset_buttons()
+
+    def set_rumble(self, left=True, value=1):
+        """Sets given rumble to given value"""
+        if left:
+            self.game_pad.setRumble(wpilib.Joystick.RumbleType.kLeftRumble,
+                                    value)
+        else:
+            self.game_pad.setRumble(wpilib.Joystick.RumbleType.kRightRumble,
+                                    value)
+        self.rumble_time = get_millis()
+
+    def check_rumble(self):
+        """Checks if rumbling has surpassed RUMBLE_LENGTH"""
+        if not isinstance(self.rumble_time, type(
+                None)) and get_millis() > self.rumble_time + RUMBLE_LENGTH:
+            # This rumbling has gone on long enough!
+            self.set_rumble(True, 0)
+            self.set_rumble(False, 0)
+            self.rumble_time = None
 
     def teleopPeriodic(self):
 
+        buttons = self.get_buttons()
+
+        if buttons[REVERSE_BUTTON]:  # Was just pressed
+            # Toggle reverse state
+            new_state = not self.button_toggles["REVERSE"]
+            self.button_toggles["REVERSE"] = new_state
+            self.set_rumble(new_state)
+            print("REVERSE")
+
+        if buttons[LOCK_BUTTON]:
+            new_state = not self.button_toggles["LOCK"]
+            self.button_toggles["LOCK"] = new_state
+            self.set_rumble(new_state)
+            print("LOCK")
+
+        self.check_rumble()
+
         axes = self.get_axes()
 
-        self.left_motor.set(axes[LEFT_AXIS])
-        self.right_motor.set(axes[RIGHT_AXIS])
+        self.left_motor.set(axes[LEFT_WHEELS_AXIS])
+        self.right_motor.set(axes[RIGHT_WHEELS_AXIS])
 
         self.robot_lift.set(axes[LIFT_AXIS])
         self.robot_arm.set(axes[ARM_AXIS])
@@ -112,13 +180,34 @@ class Robot(wpilib.IterativeRobot):
         while True:
             try:
                 value = self.game_pad.getRawAxis(i)
-                # if i == 2:
-                #     print("TRIGGER", value)
                 axes[i] = round(value, 2)
             except IndexError:
                 break
             i += 1
         return axes
+
+    def get_raw_buttons(self):
+        """Return a dictionary of raw button states"""
+        i = 1  # Controller buttons start at 1
+        buttons = {}
+        while i <= 10:
+            try:
+                value = self.game_pad.getRawButton(i)
+                buttons[i] = round(value, 2)
+            except IndexError:
+                break
+            i += 1
+        return buttons
+
+    def get_buttons(self):
+        """Returns a dictionary of bools representing whether each button was
+        just pressed
+        """
+        raw_buttons = self.get_raw_buttons()  # TODO: save button state to tell when button is first pressed rather than while button is pressed
+        for button in raw_buttons:
+            if button not in self.button_states:
+                self.button_states[button] = False
+        return self.button_states
 
     def get_axes(self):
         """Returns the current axes and values with deadzones and scaled"""
@@ -127,17 +216,42 @@ class Robot(wpilib.IterativeRobot):
         for axis in axes:
             if axis in DEADZONES:
                 value = axes[axis]
-                neg = -1 if axes[axis] < 0 else 1
+                neg = -1 if value < 0 else 1
                 deadzone = DEADZONES[axis]
                 if abs(value) > deadzone:
                     value -= deadzone * neg
                     value /= 1 - deadzone
-                    value *= SPEED_CAPS[axis][1]
+                    if neg == -1:
+                        value *= SPEED_CAPS[axis][0]
+                    else:
+                        value *= SPEED_CAPS[axis][1]
                 else:
                     value = 0
                 if REVERSES[axis]:
                     value *= -1
                 axes[axis] = value
+
+        if self.button_toggles["REVERSE"]:
+            axes[LEFT_WHEELS_AXIS] *= -1
+            axes[RIGHT_WHEELS_AXIS] *= -1
+        dif, avg = None, None
+        if self.button_toggles["LOCK"]:
+            left = axes[LEFT_WHEELS_AXIS]
+            right = axes[RIGHT_WHEELS_AXIS]
+            dif = (abs(left) + abs(right))
+            if dif <= WHEEL_LOCK and left != 0 and right != 0:
+                lneg = left < 0
+                rneg = right < 0
+                avg = dif / 2
+                axes[LEFT_WHEELS_AXIS] = avg
+                axes[RIGHT_WHEELS_AXIS] = avg
+                if lneg:
+                    axes[LEFT_WHEELS_AXIS] *= -1
+                if rneg:
+                    axes[RIGHT_WHEELS_AXIS] *= -1
+
+            # TODO: Finish fixing locking
+            print(self.button_toggles["LOCK"], left, right, axes[LEFT_WHEELS_AXIS], axes[RIGHT_WHEELS_AXIS], dif, avg)
         return axes
 
 

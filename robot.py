1a2d20e70b65f8126771226129359521cf3a6fd5
VergilTheHuragok <mathewsreece@gmail.com>
2018-03-15 20:10:53 -0500

    COMPETITION UPDATE (second attempt to upload). Switched lift to triggers. Switched arms to bumpers. Max speed control with D-PAD left/right. Down to reset max speed. Created grab button (broken currently). Added Acceleration. Added simple autonomous. Adjusted right stick output due to imprecision with controller. Fixed wheel locks. Fixed wheel direction. Various fixes. Added lots of terrible code that seems to work somehow :)

--- robot.py
+++ robot.py
@@ -4,36 +4,34 @@ import wpilib
 import wpilib.drive
 from wpilib.drive import DifferentialDrive
 
-
-# TODO: Fix triggers, bind to lift
-
 # Gamepad axes
 # TODO: dict
 LEFT_WHEELS_AXIS = 1
 RIGHT_WHEELS_AXIS = 5
-LIFT_AXIS = 0  # Should be triggers
-ARM_AXIS = 4
+LIFT_AXIS1 = 2  # Should be triggers
+LIFT_AXIS2 = 3  # Should be triggers
+POV = 11  # D-PAD port
 
 # If difference of wheel values is <=: Average two values
 WHEEL_LOCK = .1
 
 # Initiate Deadzone values
-DEFAULT_DEADZONE = .3
+DEFAULT_DEADZONE = .2
 DEADZONES = {LEFT_WHEELS_AXIS: DEFAULT_DEADZONE,
-             RIGHT_WHEELS_AXIS: DEFAULT_DEADZONE, LIFT_AXIS: .3,
-             ARM_AXIS: DEFAULT_DEADZONE}
+             RIGHT_WHEELS_AXIS: DEFAULT_DEADZONE, LIFT_AXIS1: .1}
 
 # Initiate reverse values
-DEFAULT_REVERSE = False
-REVERSES = {LEFT_WHEELS_AXIS: not DEFAULT_REVERSE,
-            RIGHT_WHEELS_AXIS: DEFAULT_REVERSE, LIFT_AXIS: True,
-            ARM_AXIS: False}
+DEFAULT_REVERSE = True
+REVERSES = {LEFT_WHEELS_AXIS: DEFAULT_REVERSE,
+            RIGHT_WHEELS_AXIS: DEFAULT_REVERSE, LIFT_AXIS1: True}
 
 # Initiate Speed caps
-DEFAULT_SPEED_CAP = [.3, .3]
+DEFAULT_SPEED_CAP = [.8, .8]
 SPEED_CAPS = {LEFT_WHEELS_AXIS: DEFAULT_SPEED_CAP,
-              RIGHT_WHEELS_AXIS: DEFAULT_SPEED_CAP, LIFT_AXIS: [.8, 0],
-              ARM_AXIS: [1, 1]}
+              RIGHT_WHEELS_AXIS: DEFAULT_SPEED_CAP, LIFT_AXIS1: [.8, 0]}
+
+SPEED_ADJUST = .1  # Amount to inc/dec max speed by with each press
+CAP_HOLD_TIME = 500  # Amount of time to hold bumpers for speed reset
 
 # PWM ports
 # TODO: Dict
@@ -45,14 +43,27 @@ ARM_PORT = 3
 GAMEPAD_NUM = 0  # This should prolly always be zero
 
 BUTTON_PORTS = {
-    "REVERSE": 1,
-    "LOCK": 3,
-    "STOP": 2,
-    "CLIMB": 4,
+    "REVERSE": 1,  # Reverse wheel dir
+    "LOCK": 3,  # Matches wheel speed when close
+    "STOP": 2,  # Stops wheels from moving
+    "SPEED": 4,  # Ignores all speed caps
+    "ARM IN": 5,  # Move arm in
+    "ARM OUT": 6,  # Move arm out
+    "INC SPEED": [11, 90],
+    "DEC SPEED": [11, 270],
+    "RES SPEED": [11, 180],
+    "GRAB": [11, 0]
 }
 
+ARM_SPEED_IN = 1
+ARM_SPEED_OUT = 1
 # Gamepad rumble length in milliseconds
 RUMBLE_LENGTH = 100
+
+AUTO_SPEED = .8
+AUTO_ACCEL = .5
+AUTO_DUR = 8000
+ACCEL = 4
 
 
 def get_millis():
@@ -81,31 +92,47 @@ class Robot(wpilib.IterativeRobot):
         self.button_toggles = None
         self.pressed_buttons = {}
         self.rumble_time = None
+        self.rumble_length = RUMBLE_LENGTH
         self.reset_buttons()
 
         # Initiate gamepad
         self.game_pad = wpilib.Joystick(GAMEPAD_NUM)
+
+        self.max_enhancer = 0
+
+        self.smart_dashboard = wpilib.SmartDashboard
+        # auto_chooser = wpilib.SendableChooser()
+        # auto_chooser.addDefault("Enable Auto", "Enable Auto")
+        # auto_chooser.addObject("Disable Auto", "Disable Auto")
+        self.smart_dashboard.putBoolean("Enable Auto", True)
+        self.auto_start_time = None
+
+        self.current_speed = [0, 0]
+        self.last_tank = 0
 
     def reset_buttons(self):
         """Resets the values of the button toggles to default likewise
         defined here
         """
         self.button_toggles = {"REVERSE": False, "LOCK": True, "STOP": False,
-                               "CLIMB": False}
+                               "SPEED": False, "GRAB": False}
         self.pressed_buttons = {}
         self.rumble_time = None
 
     def stop(self):
+        self.reset_buttons()
+        self.last_tank = get_millis()
         self.drive.stopMotor()
         self.robot_lift.stopMotor()
         self.robot_arm.stopMotor()
+        self.current_speed = [0, 0]
 
     # Events
     def robotInit(self):
         """Runs at the same time as __init__. Appears to be redundant in this
         case
         """
-        pass
+        self.stop()
 
     def robotPeriodic(self):
         pass
@@ -117,29 +144,95 @@ class Robot(wpilib.IterativeRobot):
         self.stop()
 
     def autonomousInit(self):
-        self.reset_buttons()
+        self.stop()
+        self.auto_start_time = None
+
+    def tank(self, left, right, accel=None):
+        if isinstance(accel, type(None)):
+            accel = ACCEL
+        speed1 = self.current_speed[0]
+        speed2 = self.current_speed[1]
+        if .4 > speed1 > 0:
+            if left <= 0:
+                speed1 = 0
+            else:
+                speed1 = .4
+        if -.4 < speed1 < 0:
+            if left >= 0:
+                speed1 = 0
+            else:
+                speed1 = -.4
+        if .4 > speed2 > 0:
+            if right <= 0:
+                speed2 = 0
+            else:
+                speed2 = .4
+        if -.4 < speed2 < 0:
+            if right >= 0:
+                speed2 = 0
+            else:
+                speed2 = -.4
+        if isinstance(accel, type(None)):
+            self.drive.tankDrive(left, right)
+            return None
+        vel_change = (get_millis() - self.last_tank) / 1000 * accel
+        if abs(speed1 - left) < WHEEL_LOCK:
+            speed1 = left
+        else:
+            if speed1 < left:
+                speed1 += vel_change
+            elif speed1 > left:
+                speed1 -= vel_change
+        if abs(speed2 - right) < WHEEL_LOCK:
+            speed2 = right
+        else:
+            if speed2 < right:
+                speed2 += vel_change
+            elif speed2 > right:
+                speed2 -= vel_change
+        self.current_speed = [speed1, speed2]
+        # print([round(left, 2), round(right, 2)], [round(speed1, 2), round(speed2, 2)], round(vel_change, 4))
+        self.drive.tankDrive(*self.current_speed)
+        self.last_tank = get_millis()
 
     def autonomousPeriodic(self):
-        pass
+        if self.smart_dashboard.getBoolean("Auto Enabled", True):
+            if not isinstance(self.auto_start_time, type(None)):
+                # Auto enabled and running
+                # percent = (get_millis() - self.auto_start_time) / AUTO_DUR
+                # if percent < .5:
+                #     speed = (percent/.5) * AUTO_SPEED
+                # elif percent < 1:
+                #     speed = ((1 - percent)/.5) * AUTO_SPEED
+                # else:
+                #     speed = 0
+                if get_millis() >= self.auto_start_time + AUTO_DUR / 2:
+                    self.tank(0, 0, AUTO_ACCEL)
+                else:
+                    self.tank(AUTO_SPEED, AUTO_SPEED, AUTO_ACCEL)
+            else:
+                # Auto enabled and not started
+                self.auto_start_time = get_millis()
 
     def teleopInit(self):
-        self.reset_buttons()
+        self.stop()
         self.drive.setSafetyEnabled(True)
 
-    def set_rumble(self, left=True, value=1):
+    def set_rumble(self, left=True, value=1, length=RUMBLE_LENGTH):
         """Sets given rumble to given value"""
-        if left:
+        if not left:
             self.game_pad.setRumble(wpilib.Joystick.RumbleType.kLeftRumble,
                                     value)
         else:
             self.game_pad.setRumble(wpilib.Joystick.RumbleType.kRightRumble,
                                     value)
         self.rumble_time = get_millis()
+        self.rumble_length = length
 
     def check_rumble(self):
         """Checks if rumbling has surpassed RUMBLE_LENGTH"""
         if not isinstance(self.rumble_time, type(
-                None)) and get_millis() > self.rumble_time + RUMBLE_LENGTH:
+                None)) and get_millis() > self.rumble_time + self.rumble_length:
             # This rumbling has gone on long enough!
             self.set_rumble(True, 0)
             self.set_rumble(False, 0)
@@ -149,14 +242,49 @@ class Robot(wpilib.IterativeRobot):
         """Check whether each button was just pressed and updates relevant
         toggle
         """
+        arms = 0
         button_states = self.get_buttons()
         for button_name in BUTTON_PORTS:
             port = BUTTON_PORTS[button_name]
-            if port in button_states and button_states[port]:
-                new_state = not self.button_toggles[button_name]
-                self.button_toggles[button_name] = new_state
-                self.set_rumble(new_state)
-                print(button_name, new_state)
+            angle = -1
+            if isinstance(port, list):
+                angle = port[1]
+                port = port[0]
+            if port in button_states and button_states[port] is True:
+                # button was just pressed
+                if button_name in self.button_toggles:
+                    # needs toggled
+                    if button_name == "GRAB":
+                        print(button_name, port == POV,
+                              self.get_raw_buttons()[POV], angle)
+                    if not (port == POV and not self.get_raw_buttons()[
+                                                    POV] == angle):
+                        new_state = not self.button_toggles[button_name]
+                        self.button_toggles[button_name] = new_state
+                        self.set_rumble(new_state)
+                        print(button_name, new_state)
+                elif self.get_raw_buttons()[POV] == angle:
+                    # Button angle correct, check button name
+                    if button_name == "INC SPEED":
+                        self.max_enhancer += SPEED_ADJUST
+                        self.set_rumble(True, 1)
+                    elif button_name == "DEC SPEED":
+                        self.max_enhancer -= SPEED_ADJUST
+                        self.set_rumble(False, 1)
+                    elif button_name == "RES SPEED":
+                        self.max_enhancer = 0
+                        self.set_rumble(True, length=200)
+            elif port in button_states:
+                # BUTTON BEING PRESSED
+                if button_name == "ARM IN":
+                    arms = max(-1, min(-(ARM_SPEED_IN), 1))
+                    self.button_toggles["GRAB"] = False
+                elif button_name == "ARM OUT":
+                    arms = max(-1, min(ARM_SPEED_OUT, 1))
+                    self.button_toggles["GRAB"] = False
+        if arms == 0 and self.button_toggles["GRAB"]:
+            self.robot_arm.set(ARM_SPEED_IN)
+        self.robot_arm.set(arms)
 
         self.check_rumble()
 
@@ -164,13 +292,19 @@ class Robot(wpilib.IterativeRobot):
         """Checks each axis and updates attached motor"""
         axes = self.get_axes()
         if not self.button_toggles["STOP"]:
-            self.drive.tankDrive(axes[LEFT_WHEELS_AXIS],
-                                 axes[RIGHT_WHEELS_AXIS])
+            self.tank(axes[LEFT_WHEELS_AXIS], axes[RIGHT_WHEELS_AXIS])
         # self.left_motor.set(axes[LEFT_WHEELS_AXIS])
         # self.right_motor.set(axes[RIGHT_WHEELS_AXIS])
 
-        self.robot_lift.set(axes[LIFT_AXIS])
-        self.robot_arm.set(axes[ARM_AXIS])
+        left_trigger = axes[LIFT_AXIS1]
+        right_trigger = axes[LIFT_AXIS2]
+        if left_trigger > 0:
+            if right_trigger <= 0:
+                self.robot_lift.set(-left_trigger)
+        elif right_trigger > 0:
+            self.robot_lift.set(right_trigger)
+        else:
+            self.robot_lift.set(0)
 
     def teleopPeriodic(self):
         """Runs repeatedly while robot is in teleop"""
@@ -184,6 +318,10 @@ class Robot(wpilib.IterativeRobot):
         while True:
             try:
                 value = self.game_pad.getRawAxis(i)
+                if i == RIGHT_WHEELS_AXIS:
+                    if value < 0:
+                        value /= .9  # Right stick does not go full forward
+                        value = min(value, 1)
                 if _round:
                     axes[i] = round(value, 2)
                 else:
@@ -193,31 +331,41 @@ class Robot(wpilib.IterativeRobot):
             i += 1
         return axes
 
-    def get_axes(self):
+    def get_axes(self, _round=False):
         """Returns the current axes and values with deadzones and scaled"""
-        axes = self.get_raw_axes()
+        axes = self.get_raw_axes(_round)
         # Correct deadzones and scale
         for axis in axes:
-            if axis in DEADZONES:
-                value = axes[axis]
-                neg = -1 if value < 0 else 1
-                deadzone = DEADZONES[axis]
-                if abs(value) > deadzone:
-                    value -= deadzone * neg
-                    value /= 1 - deadzone
-                    if neg == -1:
-                        value *= SPEED_CAPS[axis][0]
+            if axis != LIFT_AXIS1 and axis != LIFT_AXIS2:
+                if axis in DEADZONES:
+                    value = axes[axis]
+                    neg = -1 if value < 0 else 1
+                    deadzone = DEADZONES[axis]
+                    if abs(value) > deadzone:
+                        value -= deadzone * neg
+                        value /= 1 - deadzone
+                        if not self.button_toggles[
+                            "SPEED"]:  # Do not impose caps
+                            value *= max(-1, min(
+                                SPEED_CAPS[axis][
+                                    value >= 0] + self.max_enhancer, 1))
                     else:
-                        if not (axis == LIFT_AXIS and self.button_toggles[
-                            "CLIMB"]):
-                            # If lift and climbing enabled, allow full speed down
-                            # Down on the lift happens to use the max speed
-                            value *= SPEED_CAPS[axis][1]
-                else:
-                    value = 0
-                if REVERSES[axis]:
-                    value *= -1
-                axes[axis] = value
+                        value = 0
+                    if REVERSES[axis]:
+                        value *= -1
+                    axes[axis] = value
+            else:  # TODO: Clean up
+                # TRIGGERS FOR LIFT
+                value = axes[axis]
+                deadzone = DEADZONES[LIFT_AXIS1]
+                if value > deadzone:
+                    value -= deadzone
+                    value /= 1 - deadzone
+                    if not self.button_toggles[
+                        "SPEED"]:  # Do not impose caps
+                        value *= max(-1, min(
+                            SPEED_CAPS[LIFT_AXIS1][
+                                axis == LIFT_AXIS1] + self.max_enhancer, 1))
 
         if self.button_toggles["REVERSE"]:
             axes[LEFT_WHEELS_AXIS] *= -1
@@ -231,7 +379,7 @@ class Robot(wpilib.IterativeRobot):
                 lneg = left < 0
                 rneg = right < 0
                 smaller = min(abs(left), abs(right))
-                avg = smaller + dif
+                avg = smaller + dif / 2
                 axes[LEFT_WHEELS_AXIS] = avg
                 axes[RIGHT_WHEELS_AXIS] = avg
                 if lneg:
@@ -239,8 +387,8 @@ class Robot(wpilib.IterativeRobot):
                 if rneg:
                     axes[RIGHT_WHEELS_AXIS] *= -1
 
-            print(self.button_toggles["LOCK"], left, right,
-                  axes[LEFT_WHEELS_AXIS], axes[RIGHT_WHEELS_AXIS], dif, avg)
+            # print(self.button_toggles["LOCK"], left, right,
+            #       axes[LEFT_WHEELS_AXIS], axes[RIGHT_WHEELS_AXIS], dif, avg)
         return axes
 
     def get_raw_buttons(self):
@@ -254,6 +402,7 @@ class Robot(wpilib.IterativeRobot):
             except IndexError:
                 break
             i += 1
+        buttons[i] = self.game_pad.getPOV(0)
         return buttons
 
     def get_buttons(self):
@@ -262,11 +411,12 @@ class Robot(wpilib.IterativeRobot):
         """
         raw_buttons = self.get_raw_buttons()
         for button in raw_buttons:
-            if button not in self.pressed_buttons and raw_buttons[button]:
+            if button not in self.pressed_buttons and raw_buttons[
+                button] > 0:  # TODO: Fix up on DPAD
                 # If button not already accounted for and being pressed
                 self.pressed_buttons[button] = True
             elif button in self.pressed_buttons:
-                if raw_buttons[button]:
+                if raw_buttons[button] > 0:
                     # Being pressed, already used
                     self.pressed_buttons[button] = False
                 else:
